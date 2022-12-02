42e8dce0fb92d7caa22ed9f9e0b5dfca4880ac45
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2022-12-03 02:50:53 +0500

    22-25 task

---
+++ javaScript assignment 1/Ex 18-k/scr23.js
@@ -0,0 +1,63 @@
+/*Conditional Tests: Write a series of conditional tests. 
+Print a statement describing each test and your prediction for the
+ results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why 
+each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True
+ and another 5 tests evaluate to False. */
+
+
+ 
+ const car = "alto"
+ console.log("car === kia ? I predict False");
+ console.log(car ==='kia');
+
+
+ console.log("car === alto ? I predict True");
+ console.log(car === "alto");
+ 
+ console.log("car === Alto ? I predict False");
+ console.log(car === "Alto");
+ 
+console.log("false == 'false' ? I Predoct false");
+
+ console.log(false == 'false');
+ 
+ console.log("true === 'true' ? I predict False");
+
+ console.log(true === "true");
+
+
+
+
+ let num = 5
+ 
+ console.log("num == 5 ? I predict True");
+ console.log(num == 5);
+ 
+ console.log("num === 5 ? I predict True");
+ console.log(num === 5);
+
+ console.log("num == '5' ? I predict True");
+ console.log(num == '5');
+
+console.log("num === '5' ? I predict False");
+console.log(num === '5');
+ 
+
+ const names= "sufiyan"
+ console.log('names === "sufiyan" ? I Predict True');
+ console.log(names === "sufiyan");
+
+ console.log('names === "Sufiyan" ? I Predict False');
+ console.log(names === "Sufiyan");
+
+
---
+++ javaScript assignment 1/Ex 18-k/scr24.js
@@ -0,0 +1,31 @@
+const car = 'alto'
+console.log(car === 'alto');
+console.log(car ==='Alto');
+
+
+const num = 1000;
+console.log("num = 1000");
+console.log("testing < , > , <= , >= ");
+console.log(num < 100);
+console.log(num > 100);
+console.log(num <= 100);
+console.log(num >= 100 );
+
+console.log("number in  string testing");
+console.log(num =='1000');
+console.log(num ==='1000');
+
+console.log("Use double 'or' and 'and' operator");
+console.log(num ===100 || num === 100 );
+console.log(num == '1000' && num === 1000 );
+
+console.log("Use single 'or' and 'and' operator");
+console.log(num ===100 | num === 100 );
+console.log(num == '1000' & num === 1000 );
+
+let places = ['Karachi','Lahore','Islamabad'] 
+
+console.log(places.includes('Islamabad'));
+console.log(places.includes('Kashmir'));
+
+
