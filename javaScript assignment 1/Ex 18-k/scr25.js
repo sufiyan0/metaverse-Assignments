b4912d786d2f10238db764d1e34bcd9dd3fe3122
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2022-12-03 14:35:53 +0500

    25

---
+++ javaScript assignment 1/Ex 18-k/scr25.js
@@ -0,0 +1,42 @@
+// 25
+
+let alien_color = 'green';
+if(alien_color === 'green'){
+    console.log("The player just earned 5 points.");
+}
+
+alien_color= 'red';
+if(alien_color === 'green'){
+    console.log('\n');
+}
+
+
+
+// 26
+
+let alien = "green"
+if(alien === 'green'){
+    console.log("the player just earned 5 points for shooting the alien.");
+}else{
+    console.log("the player just earned 10 points.");
+}
+
+alien = 'red';
+if(alien === 'green'){
+    console.log("the player just earned 5 points for shooting the alien.");
+}else{
+    console.log("the player just earned 10 points.");
+}
+
+// 27
+
+if(alien === 'green'){
+    console.log("the player just earned 5 points for shooting the alien.");
+}else if(alien === 'yelow'){
+    console.log('The player earned 10 points.');
+}else if(alien === 'red'){
+    console.log('the player earned 15 points.');
+}
+
+
+
