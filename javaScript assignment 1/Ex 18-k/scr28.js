2624f59efc3d020dc4cbcf8b431c168ef2da1962
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2022-12-03 15:39:12 +0500

    31 completed

---
+++ javaScript assignment 1/Ex 18-k/scr28.js
@@ -0,0 +1,18 @@
+
+
+const age = 5;
+
+if (age < 2) {
+    console.log("Person is a baby");
+
+} else if (age >= 2 && age < 4) {
+    console.log("The person is a toddler");
+} else if (age >= 4 && age < 13) {
+    console.log("Person is a kid");
+} else if (age >= 13 && age < 20) {
+    console.log("The person is Teanager");
+} else if (age >= 20 && age < 65) {
+    console.log("The person is Adult");
+} else if (age >= 65) {
+    console.log("Person is elder");
+}
