511577954001398272c18d6253d3c3bff68cc135
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2022-12-24 00:24:24 +0500

    practice

---
+++ typescript/index.js
@@ -0,0 +1,14 @@
+function first(name) {
+    // console.log("sufiyan")
+    console.log(name);
+}
+first("sufiyan");
+function second() {
+    console.log("Never function");
+}
+function third(nam, reg) {
+    console.log(nam);
+}
+third("car", 5);
+second();
+export {};
---
+++ typescript/index.ts
@@ -0,0 +1,22 @@
+
+function first(name:string){
+    // console.log("sufiyan")
+    console.log(name)
+}
+
+first("sufiyan");
+
+function second(): void{
+    console.log("Never function")
+}
+
+function third(nam:string , reg:number):void{
+    console.log(nam)
+}
+
+
+third("car",5)
+second();
+
+
+export {}
