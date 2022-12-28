9b66e5c4ca32350bd75bda19e1f805071c60cdd6
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2022-12-28 10:40:52 +0500

    Project 02

---
+++ typescript/project02/index.ts
@@ -0,0 +1,76 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt(
+    
+    [
+    {
+        type:"number",
+        name:"UserId",
+        message:"Enter Your id here"
+    },
+    {
+        type:"number",
+        name:"pin",
+        message:"Enter Your Pic here"
+    },
+    {
+        type:"list",
+        name:"accType",
+        choices:["current Account" , "Saving Account"],
+        message:"Chose your account type.",
+        when(answer){
+            return answer.UserId && answer.pin;
+        }
+    },
+    {
+        type:"list",
+        name:"operation",
+        choices:["withdrow","Transfer","BalanceQuery"],
+        message:"What operation You want to perform",
+        when(answer){
+            return answer.accType;
+        }
+    },
+    {
+        type:"number",
+        message:"Enter The ammount you want to withdrow",
+        name:"amountWithdrow",
+        when(answer){
+            return answer.operation == "withdrow"
+        }
+    },
+    {
+        type:"number",
+        name:"amountTransfer",
+        message:"Enter The ammount you want to Transfer",
+        when(answer){
+            return answer.operation == "Transfer"
+        }
+    }
+   
+
+])
+const balance = Math.floor(Math.random()*1000000) ;
+
+const {withdrow, Transfer,BalanceQuery, amountWithdrow,amountTransfer} = answer
+if(withdrow){
+    if(balance >= amountWithdrow){
+        console.log(`Your previous Balance is: ${balance}`);
+        let newBalance = balance - amountWithdrow;
+        console.log(`Your current Balance is: ${newBalance}`);
+    }else{
+        console.log("Insufficient Balance")
+    }
+}else if(Transfer){
+    if(balance >= amountTransfer){
+        console.log(`Your previous Balance is: ${balance}`);
+        let newBalance = balance - amountTransfer;
+        console.log(`Your current Balance is: ${newBalance}`);
+    }else{
+        console.log("Insufficient Balance")
+    }
+}else if(BalanceQuery){
+    console.log(`Your current Balance is: ${balance}`);
+}
+
+
